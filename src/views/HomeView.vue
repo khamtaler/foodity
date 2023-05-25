53efb561fec20c7be6d56625c9fad5efd7a02bbc
Tomasz Sroka <tsroka41@gmail.com>
2023-05-25 14:24:50 +0200

    finalizing vuex store setup and tailwind setup as well

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,15 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <pre>{{ meals }}</pre>
   </main>
 </template>
+
+<script setup>
+import { computed } from 'vue'
+import store from '../store'
+
+const meals = computed(() => {
+  return store.state.meals
+})
+</script>
